035fb630cf9da796bb6f8f0123beea834a04a86e
Ryota Ishidu <ishidu-ryota-fz@ynu.jp>
2017-10-21 18:47:40 +0900

    ｔｍｐ

--- parameters.h
+++ parameters.h
@@ -2,11 +2,11 @@
 
 #define RECALL_TIME 10000
 #define DELTA_T 0.005
-#define	NOISE 1.0
-#define U0 0.02
-#define A 4.0
-#define B 4.0
-#define D 2.0
+#define	NOISE 0.00001
+#define U0 0.05
+#define A 2.0
+#define B 2.0
+#define D 1.0
 
 const std::vector<std::vector<double>> cities = {
 	{ 0.5, 0.9 },
